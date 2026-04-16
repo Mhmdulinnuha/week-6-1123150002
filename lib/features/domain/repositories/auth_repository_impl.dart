ae91765fd10ad5762a1d3a21e881b636d18c158f
Mhmdulinnuha <muhamadulinnuha653@gmail.com>
2026-04-16 23:41:42 +0700

    membuat auth repository dan impl

---
+++ lib/features/domain/repositories/auth_repository_impl.dart
@@ -0,0 +1,17 @@
+import '../repositories/aut_repository.dart';
+import '../../../core/services/dio_client.dart';
+import '../../../core/contstans/api_constants.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<String> verifyFirebaseToken(String firebaseToken) async {
+    final response = await DioClient.instance.post(
+      ApiConstants.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+
+
+    final data = response.data['data'] as Map<String, dynamic>;
+    return data['access_token'] as String;
+  }
+}
